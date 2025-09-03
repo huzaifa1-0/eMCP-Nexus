9e888a7b8dd0b9e3c47aee9fa6e904a08eaacbd6
Huzaifa Rashid <huzaifaras10@gmail.com>
2025-09-03 13:13:46 -0700

    .

---
+++ backend/db.py
@@ -0,0 +1,14 @@
+import databases
+import sqlalchemy
+from backend.config import settings
+
+DATABASE_URL = settings.database_url
+
+database = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+engine = sqlalchemy.create_engine(DATABASE_URL)
+metadata.create_all(engine)
+
+
+async def init_db():
+    await database.connect()
