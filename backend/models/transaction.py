8723e3732599824faf1ccf4c384a36f5d5d1ab39
Huzaifa Rashid <huzaifaras10@gmail.com>
2025-09-04 12:16:53 -0700

    done

--- backend/models/transaction.py
+++ backend/models/transaction.py
@@ -2,3 +2,11 @@ from pydantic import BaseModel
 from datetime import datetime
 
 
+class Transaction(BaseModel):
+    id: int
+    tool_id: int
+    user_id: int
+    amount: float
+    currency: str
+    method: str
+    timestamp: datetime
