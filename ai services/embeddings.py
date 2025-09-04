576cecb0002dc2e6c75d6e9d775887462d5e1c24
Huzaifa Rashid <huzaifaras10@gmail.com>
2025-09-04 12:49:20 -0700

    .

--- ai services/embeddings.py
+++ ai services/embeddings.py
@@ -1,3 +1,11 @@
 from sentence_transformers import SentenceTransformer
 import numpy as np
 
+embedding_model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+
+
+def get_embedding(text: str) -> np.ndarray:
+    """
+    Converts input text into an embedding vector using HuggingFace.
+    """
+    return embedding_model.encode(text, normalize_embeddings=True).tolist()
