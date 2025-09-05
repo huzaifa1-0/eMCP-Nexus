b50d00dbae25506e1d8de97fcb271497120bc397
Huzaifa Rashid <huzaifaras10@gmail.com>
2025-09-05 05:39:45 -0700

    .

--- backend/db.py
+++ backend/db.py
@@ -1,14 +1,22 @@
-import databases
 import sqlalchemy
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from backend.config import settings
 
 DATABASE_URL = settings.DATABASE_URL
 
-database = databases.Database(DATABASE_URL)
-metadata = sqlalchemy.MetaData()
-engine = sqlalchemy.create_engine(DATABASE_URL)
-metadata.create_all(engine)
+
+engine = create_async_engine(DATABASE_URL)
+async_session_factory = async_sessionmaker(
+    engine, 
+    expire_on_commit=False, 
+    class_=AsyncSession
+)
+
+async def get_async_session():
+    async with async_session_factory() as session:
+        yield session
 
 
 async def init_db():
-    await database.connect()
+    async with engine.begin() as conn:
+        await conn.run_sync(sqlalchemy.MetaData().create_all)
