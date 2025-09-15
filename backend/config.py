730a8e442f0763caaf16e4b1a81012ccba3eed95
Huzaifa Rashid <huzaifaras10@gmail.com>
2025-09-15 14:11:38 -0700

    .

--- backend/config.py
+++ backend/config.py
@@ -6,8 +6,6 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str = "postgresql://admin:passsword@db:5432/marketplace"
-    GOOGLE_PROJECT: str
-    GOOGLE_DATASET: str
     STRIPE_KEY: str
     COINBASE_KEY: str
     SECRET_KEY: str = "your-secret-key"  
