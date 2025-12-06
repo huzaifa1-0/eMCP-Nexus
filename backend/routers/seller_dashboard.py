97fa6f1fec32bde59f8c0711f319cc784e672f04
Huzaifa Rashid <huzaifaras10@gmail.com>
2025-12-06 11:57:22 -0800

    .

--- backend/routers/seller_dashboard.py
+++ backend/routers/seller_dashboard.py
@@ -2,3 +2,5 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select, func
 from typing import List, Dict, Any
+
+from backend.db import get_async_session
